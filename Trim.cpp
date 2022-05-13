9911fc1e6f7158f7d24eff6a47a7af392dc60b41
lichrist <lichrist@hawaii.edu>
2022-05-13 10:10:10 -1000

    Created the functions and added some rules

---
+++ Trim.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Christian Li on 5/13/22.
+//
+#include <string>
+
+#include "Trim.h"
+
+using namespace std;
+
+unsigned int string_length( const char inString[] ){
+    if(inString == nullptr)
+        return 0;
+
+    return inString[];
+}
+
+const char* trim_left( const char inString[] ){
+    if(inString == nullptr)
+        return nullptr;
+
+}
+
+char* map_chars( const char inString[], char buffer[] ){
+
+}
---
+++ Trim.h
@@ -0,0 +1,8 @@
+//
+// Created by Christian Li on 5/13/22.
+//
+
+#ifndef EE205_FINAL_EXAM_TRIM_H
+#define EE205_FINAL_EXAM_TRIM_H
+
+#endif //EE205_FINAL_EXAM_TRIM_H
